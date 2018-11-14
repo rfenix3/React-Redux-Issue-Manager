380991b630bfe62b3352c2f66367a93fa064f20d
rfenix3 <richard.fenix@gmail.com>
2018-11-14 10:56:15 -0800

    Create react framework and display sample data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IssueFilter from './components/issueFilter'
+import IssueTable from './components/issueTable'
+import IssueAdd from './components/issueAdd'
+
+
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { issues: issues };
+
+    //this.createIssue = this.createIssue.bind(this);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Issue Tracker</h1>
+        < IssueFilter />
+        <hr />
+        <IssueTable issues={this.state.issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue}/>
       </div>
     );
   }
